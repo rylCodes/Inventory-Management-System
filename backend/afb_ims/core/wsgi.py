fa77072e5415d10f3207b2ceaa123c3525d00bc7
rylCodes <andresdaryl.94@gmail.com>
2023-12-22 21:24:12 +0800

    same as last commit

--- backend/afb_ims/core/wsgi.py
+++ backend/afb_ims/core/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# settings_module = 'core.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'core.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+settings_module = 'core.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'core.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()

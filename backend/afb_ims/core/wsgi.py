d97e3ffca8abf190eb734e528d14f2668fcdb61a
rylCodes <andresdaryl.94@gmail.com>
2023-12-22 21:19:23 +0800

    override the existing code

--- backend/afb_ims/core/wsgi.py
+++ backend/afb_ims/core/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'core.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'core.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+# settings_module = 'core.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'core.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()

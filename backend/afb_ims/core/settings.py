abf832f00250ee16039d55b1fbb4a22623e5467f
rylCodes <andresdaryl.94@gmail.com>
2023-12-24 23:39:49 +0800

    configure azure postgresql db

--- backend/afb_ims/core/settings.py
+++ backend/afb_ims/core/settings.py
@@ -94,6 +94,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # }
 
 DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "afb-db",
+        "USER": "inveniadb",
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": "invenia-postgresql.postgres.database.azure.com",
+        "PORT": "5432",
+        "OPTIONS": {"sslmode": "require"},
+    }
+}
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
